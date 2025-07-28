4860dae25781df8bd6e2b04998675afaa0d913dc
Igorase <garik.sushik@ya.ru>
2025-07-28 22:44:43 +0500

    Create DatabaseConfig and some reforges.

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 
 
 class RunConfig(BaseModel):  # BaseModel задает структуру данных
@@ -11,9 +12,18 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseSettings):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):  # BaseSettings задает основные настройки, ну также может там из файликов брать .env данные
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
